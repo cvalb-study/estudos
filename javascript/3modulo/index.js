22472214336b9966ca5242584c442d0e80e1ffaf
Carlos Valbusa <carlos.valbusa@gmail.com>
2020-11-23 22:55:40 -0300

    Organizing folders

--- javascript/3modulo/index.js
+++ javascript/3modulo/index.js
@@ -2,6 +2,7 @@ import { Cliente } from "./Cliente.js";
 import { Gerente } from "./Funcionario/Gerente.js";
 import { Diretor } from "./Funcionario/Diretor.js";
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
+import { ContaCorrente } from './Conta/ContaCorrente.js';
 
 const diretor = new Diretor("Rodrigo", 10000, 12345678900);
 diretor.cadastrarSenha("123456");
@@ -10,8 +11,10 @@ gerente.cadastrarSenha("123");
 
 const cliente = new Cliente("Lais", 78945612398, "456");
 
+const conta = new ContaCorrente("Gustavo", 1672);
+
 const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "123");
 const diretorEstaLogado = SistemaAutenticacao.login(diretor, "123456");
 const clienteEstaLogado = SistemaAutenticacao.login(cliente, "456");
 
-console.log(clienteEstaLogado);
+console.log(conta.saldo);
